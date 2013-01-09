57acec8fe7a6f2a31d0bdf87b2dfbf5ef915a86f
catcat <catcat@antireg.ru>
2013-01-09 15:24:56 +0200

    settings

--- src/main/java/org/example/controller/PublicController.java
+++ src/main/java/org/example/controller/PublicController.java
@@ -2,30 +2,38 @@ package org.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.core.io.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
-//import javax.annotation.Resource;
+
 import org.example.dao.SimpleDao;
+
+
 
 @Controller
 public class PublicController {
 
     //private static final Logger log = Logger.getLogger(PublicController.class);
 
-    @Autowired
-    WebApplicationContext context;
 
     @Autowired
-    SimpleDao simpleDao;
+    private WebApplicationContext context;
+
+    @Autowired
+    private SimpleDao simpleDao;
+
+    @Autowired
+    private WebApplicationContext ctx;
 
 
-    /*
-    @Resource(name="example.pdf")
-    Resource injRes;
-    */
+
 
     @RequestMapping("/document.htm")
     public ModelAndView documentPage(@RequestParam("key") String key) throws Exception {
@@ -36,6 +44,12 @@ public class PublicController {
         return info;
     }
 
+
+    @RequestMapping("/ya.htm")
+    public void ya(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Resource webpage = ctx.getResource("http://ya.ru");
+        FileCopyUtils.copy(webpage.getInputStream(), response.getOutputStream());
+    }
 
     private ModelAndView getErrorMav(String message) {
         ModelAndView error =  new ModelAndView("error");
