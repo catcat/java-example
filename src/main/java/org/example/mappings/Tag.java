840fd6d5c41f6af2938d87807230973631e2b1ea
catcat <1@1.net>
2013-02-04 02:34:58 +0200

    tag

---
+++ src/main/java/org/example/mappings/Tag.java
@@ -0,0 +1,66 @@
+package org.example.mappings;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Tag {
+    private int id;
+
+    @javax.persistence.Column(name = "id")
+    @Id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String name;
+
+    @javax.persistence.Column(name = "name")
+    @Basic
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String description;
+
+    @javax.persistence.Column(name = "description")
+    @Basic
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        if (id != tag.id) return false;
+        if (description != null ? !description.equals(tag.description) : tag.description != null) return false;
+        if (name != null ? !name.equals(tag.name) : tag.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
